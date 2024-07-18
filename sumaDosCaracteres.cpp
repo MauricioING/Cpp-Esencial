01ffcd5a6cffd3a02ed64769d6018ac229e4cda2
Mauricio <mauricio@ingenieriainversa.cl>
2024-07-17 22:47:38 -0400

    Create sumaDosCaracteres.cpp

---
+++ sumaDosCaracteres.cpp
@@ -0,0 +1,12 @@
+// Completa el siguiente programa realizando la suma de dos caracteres
+
+int main(){
+    
+    /* 
+       Escribe
+       tu solucion
+       aqui
+    */
+    
+    return 0;
+}
